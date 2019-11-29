74ad1bf42dd1d079dff43ef6a240babadbb4a493
Clint Diamante <clintdiamante@gmail.com>
2019-11-29 17:56:55 +0800

    Initial Commit. Created welcome and project section.

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+// Welcome Navigation
+  $('#nav-link-projects').on('click', function()
+    {
+      $('#projects').show();
+
+      $('#welcome-section').hide();
+      $('#about').hide();
+      $('#contact').hide();
+    }
+  );
