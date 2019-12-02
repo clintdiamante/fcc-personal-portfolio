082d4ffdaee18c6e3f41bd9b5e56704f832a7987
Clint Diamante <clintdiamante@gmail.com>
2019-12-02 09:28:25 +0800

    Added project list viewing options.

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,3 +8,18 @@
       $('#contact').hide();
     }
   );
+
+// Projects View
+  $('#projects-compact').on('click', function()
+    {
+      $('#projects-full-view').hide();
+      $('#projects-compact-view').show();
+    }
+  );
+
+  $('#projects-full').on('click', function()
+    {
+      $('#projects-full-view').show();
+      $('#projects-compact-view').hide();
+    }
+  );
