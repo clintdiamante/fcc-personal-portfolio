276fe7a90ba9dc34e6aefde339b0725fb625d52e
Clint Diamante <clintdiamante@gmail.com>
2019-12-26 14:03:53 +0800

    Completed contact section. Disabled the jQuery show/hide tricks. 12/12 pass.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,50 @@
-// Welcome Navigation
-  $('#nav-link-projects').on('click', function()
+/*
+  I had to disable this T_T
+*/
+
+// Main Navigation
+  // WELCOME PAGE TO PROJECT PAGE
+/*  $('#nav-link-projects').on('click', function()
     {
       $('#projects').show();
-
       $('#welcome-section').hide();
-      $('#about').hide();
       $('#contact').hide();
     }
-  );
-
-  $('#projects-sub-navbar-back').on('click', function()
+  );*/
+  // PROJECT PAGE TO WELCOME PAGE
+/*  $('#projects-sub-navbar-back').on('click', function()
     {
       $('#welcome-section').show();
       $('#projects').hide();
     }
-    );
+  );*/
+  // WELCOME PAGE TO CONTACT PAGE
+/*  $('#nav-link-contact').on('click', function()
+    {
+      $('#contact').show();
+      $('#welcome-section').hide();
+      $('#projects').hide();
+    }
+  );*/
+  // CONTACT PAGE TO WELCOME PAGE
+/*  $('#contact-sub-navbar-back').on('click', function()
+    {
+      $('#welcome-section').show();
+      $('#contact').hide();
+    }
+  );*/
 
 // Projects View
-  $('#projects-compact').on('click', function()
+/*  $('#projects-compact').on('click', function()
     {
       $('#projects-full-view').hide();
       $('#projects-compact-view').show();
     }
-  );
+  );*/
 
-  $('#projects-full').on('click', function()
+/*  $('#projects-full').on('click', function()
     {
       $('#projects-full-view').show();
       $('#projects-compact-view').hide();
     }
-  );
+  );*/
